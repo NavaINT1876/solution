8e1f1e781b904c1d74013fbdf2a851570ba131e2
Igor <igor.kucherenko@mail.ru>
2016-10-23 13:26:23 +0300

    refactored

--- index.php
+++ index.php
@@ -40,70 +40,43 @@ function solution($integerX, $arrayA)
 
     $elementsAmount = count($arrayA);
 
-    $XElementsAmount = 0;
-    foreach ($arrayA as $key => $item) {
-        if (!is_int($item) || $item < MINIMUM_X || $item > MAXIMUM_X) {
-            return -1;
-        }
-        if ($item === $integerX) {
-            $XElementsAmount++;
-        }
-    }
+    $arrayAChunk = array_chunk($arrayA, ceil($elementsAmount / 2), true);
 
-    if ($XElementsAmount == 1 && $integerX == $arrayA[$elementsAmount - 1]) {
-        return -1;
-    } elseif ($XElementsAmount == 1 && $integerX != $arrayA[$elementsAmount - 1]) {
-        return count($arrayA) - 1;
-    } else {
-        $equalsAmount = 0;
-        $notEqualsAmount = 0;
+    $firstArr = $arrayAChunk[0];
+    $secondArr = $arrayAChunk[1];
 
-        $firstArr = array_chunk($arrayA, ceil($elementsAmount / 2), true)[0];
-        $secondArr = array_chunk($arrayA, ceil($elementsAmount / 2), true)[1];
+    $equalsAmount = array_count_values($firstArr)[$integerX] ?:0;
+    $notEqualsAmount = count($secondArr) - array_count_values($secondArr)[$integerX] ?:0;
 
-        foreach ($firstArr as $fItem) {
-            if ($fItem == $integerX) {
-                $equalsAmount++;
-            }
-        }
-
-        foreach ($secondArr as $sItem) {
-            if ($sItem != $integerX) {
-                $notEqualsAmount++;
-            }
-        }
-
-        if ($equalsAmount == $notEqualsAmount) {
-            return count($firstArr);
-        } elseif ($equalsAmount < $notEqualsAmount) {
-            while ($equalsAmount < $notEqualsAmount) {
-                $firstElemOfSecondArr = array_shift($secondArr);
-                array_push($firstArr, $firstElemOfSecondArr);
-                if ($firstElemOfSecondArr == $integerX) {
-                    $equalsAmount++;
-                } else {
-                    $notEqualsAmount--;
-                }
-            }
-        } else {
-            while ($equalsAmount > $notEqualsAmount) {
-                $lastElemOfFirstArr = array_pop($firstArr);
-
-                array_unshift($secondArr, $lastElemOfFirstArr);
-                if ($lastElemOfFirstArr != $integerX) {
-                    $notEqualsAmount++;
-                } else {
-                    $equalsAmount--;
-                }
-            }
-        }
-
-        if ($equalsAmount == 0 || $notEqualsAmount == 0) {
-            return -1;
-        }
-
+    if ($equalsAmount != 0 && $equalsAmount == $notEqualsAmount) {
         return count($firstArr);
+    } elseif ($equalsAmount < $notEqualsAmount) {
+        while ($equalsAmount < $notEqualsAmount) {
+            $firstElemOfSecondArr = array_shift($secondArr);
+            array_push($firstArr, $firstElemOfSecondArr);
+            if ($firstElemOfSecondArr == $integerX) {
+                $equalsAmount++;
+            } else {
+                $notEqualsAmount--;
+            }
+        }
+    } else {
+        while ($equalsAmount > $notEqualsAmount) {
+            $lastElemOfFirstArr = array_pop($firstArr);
+            array_unshift($secondArr, $lastElemOfFirstArr);
+            if ($lastElemOfFirstArr != $integerX) {
+                $notEqualsAmount++;
+            } else {
+                $equalsAmount--;
+            }
+        }
     }
+
+    if ($equalsAmount == 0 || $notEqualsAmount == 0) {
+        return -1;
+    }
+
+    return count($firstArr);
 }
 
 echo solution($integerX, $arrayA);
